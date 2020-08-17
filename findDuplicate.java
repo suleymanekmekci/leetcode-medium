4e8f921cf44053fe9e478f16b84e93d0a556079e
Süleyman Ekmekçi <63970122+suleymanekmekci@users.noreply.github.com>
2020-08-17 15:12:26 +0300

    287. Find the Duplicate Number
    https://leetcode.com/problems/find-the-duplicate-number/

---
+++ findDuplicate.java
@@ -0,0 +1,27 @@
+/** 287. Find the Duplicate Number 
+ * https://leetcode.com/problems/find-the-duplicate-number/
+ * Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+ *
+ * Example 1:
+ *
+ * Input: [1,3,4,2,2]
+ * Output: 2
+ * Example 2:
+ *
+ * Input: [3,1,3,4,2]
+ * Output: 3
+ */
+class findDuplicate {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+
+        for (int i =1 ; i< nums.length ; i++) {
+            if (nums[i] == nums[i-1]) {
+                return nums[i];
+            }
+        }
+        return -1;
+
+    }
+
+}
